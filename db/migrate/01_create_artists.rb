869f1272592a1ef29fb3eb258b0dea02162a3aba
Marianne Pyon <mariannepyon@gmail.com>
2019-10-14 18:53:35 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,12 +1,11 @@
-class CreateArtists < ActiveRecord::Migration[5.2]
-  def up
-  end
 
-  def down
+  class CreateArtists < ActiveRecord::Migration[5.2]
+    def change
+      create_table :artists do |t|
+        t.string :name
+        t.string :genre
+        t.integer :age
+        t.string :hometown
+      end
+    end
   end
-end
-
-def change
-  create_table :artists do |t|
-  end
-end
